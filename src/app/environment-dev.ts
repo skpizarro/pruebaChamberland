4c7edca0a0ed5038cdb784f2fccb97f68fec47bb
Juan Pablo Pizarro <juan.pizarro@ias.com.co>
2024-07-04 21:00:36 -0500

    Se realizan los servicios de departamento y municipio

--- src/app/environment-dev.ts
+++ src/app/environment-dev.ts
@@ -1,4 +1,4 @@
 export const environment =
 {
-  BASE_URL:'http://localhost:8080'
+  BASE_URL:'http://localhost:8080/api-chamber/v1'
 }
--- src/app/modules/services/info-lists.service.ts
+++ src/app/modules/services/info-lists.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environment-dev';
 import { HttpClient } from '@angular/common/http';
 import { Municipality } from '../interfaces/municipality.interface';
+import { Department } from '../interfaces/department.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,12 +14,14 @@ export class InfoListsService {
 
   constructor(private httpClient: HttpClient) { }
 
-  findDepartments():Department[]{
-
+  findDepartmentsList():Observable<Department[]> {
+    const uri = `${this.BASE_URL}/department/list`
+    return this.httpClient.get<Department[]>(uri);
   }
 
-  findMunicipalities(cdDepartment:number):Municipality[]{
-
+  findMunicipalitiesListByDepartment(cdDepartment:number):Observable<Municipality[]> {
+    const uri = `${this.BASE_URL}/municipality/list/${cdDepartment}`
+    return this.httpClient.get<Municipality[]>(uri);
   }
 
 }
